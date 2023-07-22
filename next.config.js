3f03cd9010c5eff0fed9e403d3ee8849bb99024c
clabby <ben@clab.by>
2023-07-22 03:00:01 -0400

    SSR for `styled-components`

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: config => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
